e5556b5a8a7ad3ac6c38469c0bc7f240f8e16e0b
Jeff Kunkle <jeff.kunkle@nearinfinity.com>
2012-01-30 20:33:20 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+//exports.html = require('./lib/format/html');
+//exports.json = require('./lib/format/json');
+//exports.plain = require('./lib/format/json');
+exports.format = require('./lib/format');
+exports.map = require('./lib/map');
