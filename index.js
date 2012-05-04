912cc3d64c94d84d6aedc7392d7fbd80e23501fe
Joe Ferner <joe@fernsroth.com>
2012-05-04 10:04:51 -0400

    added merge capability for multiple files

--- index.js
+++ index.js
@@ -1,5 +1,40 @@
-//exports.html = require('./lib/format/html');
-//exports.json = require('./lib/format/json');
-//exports.plain = require('./lib/format/json');
+var fs = require('fs');
+var merge = require('./lib/merge');
+var async = require('async');
+
 exports.format = require('./lib/format');
 exports.map = require('./lib/map');
+
+exports.run = function (options, callback) {
+  callback = callback || function () {};
+  var mapper = require('./lib/map/' + options.map);
+  var formatter = require('./lib/format/' + options.f);
+
+  var files = options._;
+  if (files.length === 0) {
+    var buffers = [];
+    process.stdin.on('data', buffers.push.bind(buffers));
+    process.stdin.on('error', function (err) {
+      console.error(err);
+      process.exit(1);
+    });
+    process.stdin.on('end', function () {
+      var data = buffers.join('');
+      var mappedOutput = mapper.map(data);
+      formatter.report(mappedOutput, options.write);
+      callback();
+    });
+    process.stdin.resume();
+  } else {
+    async.mapSeries(files, fs.readFile, function (err, results) {
+      if (err) {
+        console.error(err);
+        return process.exit(1);
+      }
+      results = results.map(function (buffer) {return mapper.map(buffer.toString()); });
+      var data = merge(results);
+      formatter.report(data, options.write);
+      callback();
+    });
+  }
+};
